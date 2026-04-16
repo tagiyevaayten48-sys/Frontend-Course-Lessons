dbd566979e3a609c91be9c33796cfaa6106f2737
tagiyevaayten48 <tagiyevaayten48@gmail.com>
2026-04-16 19:02:49 +0400

    All Lessons

---
+++ Javascript/app.js
@@ -0,0 +1,35 @@
+//var x = 5
+//var y = 10
+//var z=x+y
+//console.log(z)
+//console.log(typeof(z))
+// var x ="5"
+// var y = 15
+// var z = x+y
+//console.log(z)
+// var x
+// var y = null
+//var Varad=[5,"Java",true]
+//consile.log 
+
+
+//console.log(5+5)
+//ders 3
+// 1. Aritmetik - riyazi əməliyyatları +,-,/,%,x++,++x,x--,--x
+// 2. Desigment operator +=,-=,*=,%=,**=
+// 3. Comparison operator =,==,===,!=,!==
+// 4. Logical operator and,or,not
+let cars = [
+    {
+        marka: "Hunday",
+        model: "A5",
+        year: 2026
+    },
+    {
+        marka: "BMV",
+        model: "05",
+        year: 2025
+    }
+]
+console.log(cars[1].model)
+   
---
+++ Javascript/yeni11/Dom.js
@@ -0,0 +1,6 @@
+
+//Modify elements classname & Atribute
+let exp = document.getElementById("example")
+exp.classList.add("Hello")
+exp.classList.remove("Hello")
+console.log(exp)
